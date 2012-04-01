504cdf30b3aa3958cca620ac642a82f7e19057a6
Jasdeep Jaitla <jasdeep@rockstrs.com>
2012-03-31 22:03:53 -0700

    first commit

--- enum.rb
+++ enum.rb
@@ -1,16 +1,16 @@
 # Enum Parent Class for emulating Enum's of other strong typed languages like C++, C# and Java
 # Usage: 
 # => class MyEnum < Enum
-# => 		enum_attr :val1, 1
-# => 		enum_attr :val2, 2
+# =>    enum_attr :val1, 1
+# =>    enum_attr :val2, 2
 # => end
 #
 # e = MyEnum.new MyEnum::VAL2 # => #<MyEnum:0x007f968b2bef20 @attrs=2>
-# e.val2?	# => true
-# e.val1?	# => false
-# e.to_i	# => 2
+# e.val2? # => true
+# e.val1? # => false
+# e.to_i  # => 2
 # e.val1 = true # => true
-# e.val1?	# => true
+# e.val1? # => true
 #
 # m = MyClass.new {myvar => "my var value", myenum => MyEnum::VAL1 }
 #
@@ -18,11 +18,11 @@ class Enum
 
   private
 
-	# define instance methods based on enumeration passed in
+  # define instance methods based on enumeration passed in
   def self.enum_attr(name, num)
     name = name.to_s
 
-		# create class constant
+    # create class constant
     const_set(name.upcase, num)
 
     # create attribute get method
@@ -35,7 +35,7 @@ class Enum
       @attrs & num != 0
     end
     
-		# create attribute = set method
+    # create attribute = set method
     define_method(name + '=') do |set|
       if set
         @attrs |= num
@@ -48,7 +48,7 @@ class Enum
 
   public
 
-	# can pass in Fixnum/Integer, a predefined symbol, an array of symbols
+  # can pass in Fixnum/Integer, a predefined symbol, an array of symbols
   def initialize(attrs = 0)
     if attrs.is_a?(Fixnum) || attrs.is_a?(Integer)
       @attrs = attrs
@@ -69,15 +69,15 @@ class Enum
     @attrs
   end
 
-	# NOTE: this only works for non-bit based enumerations, combinatorial enumerations will return nil 
-	def to_s
-		constants = instance_eval self.class.to_s + "::constants"
-	  constants.each do |constant|
-	  	const = self.class.to_s + "::" + constant.to_s
-	    if @attrs == instance_eval(const)
-	    	return const
-	    end
-	  end
-		nil
-	end
+  # NOTE: this only works for non-bit based enumerations, combinatorial enumerations will return nil 
+  def to_s
+    constants = instance_eval self.class.to_s + "::constants"
+    constants.each do |constant|
+      const = self.class.to_s + "::" + constant.to_s
+      if @attrs == instance_eval(const)
+        return const
+      end
+    end
+    nil
+  end
 end
